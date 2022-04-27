a01f822331250c19055744b5dc9f87faaf2ca7e8
박진홍 <jinhongpa123@naver.com>
2022-04-27 18:50:00 +0900

    220427

---
+++ python/practice.py
@@ -0,0 +1,22 @@
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print('풍선')
+# print("풍선")
+# print("ㅋㅋㅋㅋㅋㅋ")
+# print("ㅋ"*5)
+# print(5 > 10)
+# print(5 < 10)
+# print(True)
+# print(not 5 < 10)
+# print(not 5 > 10)
+
+name = "박진홍"
+age = 25
+is_adult = age >= 3
+
+print("나는 "+name+"입니다")
+print("저는 "+str(age)+"살입니다")
+print("저는 어른일까요?" + str(is_adult))
