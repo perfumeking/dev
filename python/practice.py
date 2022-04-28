8e544399713df1762007e41a417908350d2e3d53
박진홍 <jinhongpa123@naver.com>
2022-04-28 23:29:14 +0900

    220428

--- python/practice.py
+++ python/practice.py
@@ -13,10 +13,45 @@
 # print(not 5 < 10)
 # print(not 5 > 10)
 
-name = "박진홍"
-age = 25
-is_adult = age >= 3
+# name = "박진홍"
+# age = 25
+# is_adult = age >= 3
 
-print("나는 "+name+"입니다")
-print("저는 "+str(age)+"살입니다")
-print("저는 어른일까요?" + str(is_adult))
+# print("나는 "+name+"입니다")
+# print("저는 "+str(age)+"살입니다")
+# print("저는 어른일까요?" + str(is_adult))
+
+# station = "사당"
+# print(station + "행 열차가 들어오고 있습니다.")
+# station = "신도림"
+# print(station + "행 열차가 들어오고 있습니다.")
+# station = "인천공항"
+# print(station + "행 열차가 들어오고 있습니다.")
+
+
+# print(1+1)
+# print(2**3) # 2^3
+# print(2^3)
+# print(10%3)
+# print(10//3)
+
+# number = 4 
+# print(number)
+# number += 2
+# print(number)
+# number -= 2
+# print(number)
+# print(abs(-4))
+# print(pow(6,3))
+# print(max(3,4,5,6,7,11,1))
+# print(min(3,4,5,6,1,8))
+# print(round(3.14))
+# from math import *
+# print(floor(4.99))
+# print(ceil(3.14))
+# print(sqrt(16))
+
+from random import *
+print(random()*100) # 0.0 ~ 1.0 무작위
+print(random() * 10) # 0.0 ~ 10.0
+print(int(random()*10))
